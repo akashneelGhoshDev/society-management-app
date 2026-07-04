ad3f14987272ee059d93f4ada42ba951e636051f
akashneelGhoshDev <akashneel.ghosh.enterprises@gmail.com>
2026-07-04 23:08:24 +0530

    feat(electron): integrate Electron for desktop application support with main process setup

---
+++ society-management-ui/electron/main.js
@@ -0,0 +1,18 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1400,
+    height: 900,
+    autoHideMenuBar: true,
+    webPreferences: {
+      contextIsolation: true,
+      nodeIntegration: false,
+    },
+  });
+
+  win.loadFile(path.join(__dirname, '../dist/society-management-ui/browser/index.html'));
+}
+
+app.whenReady().then(createWindow);
